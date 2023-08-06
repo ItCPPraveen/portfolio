db01bb63e08cf0e805ccd992e7ae13a9ac4f9aba
praveen <cppraveen02@gmail.com>
2023-08-06 22:13:16 +0530

    updated assets and homo layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutmeComponent } from './aboutme/aboutme.component'
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'aboutme', component: AboutmeComponent },
+    { path: '', component: HomeComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
